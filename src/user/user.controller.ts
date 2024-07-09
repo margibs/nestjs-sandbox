06e1fdf6545cd37439d3048fccd508dd7be2f64d
Rumar Gibs Ecaldre <margibs_hbb@yahoo.com>
2024-07-09 18:04:57 +0800

    Added rest logic

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,14 +1,46 @@
-import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UserService } from './user.service';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('users')
 export class UserController {
+  constructor(private readonly userService: UserService) {}
+
   @Get()
-  getUsers(): [] {
-    return [];
+  findAll() {
+    return this.userService.findAll();
   }
 
   @Get(':id')
-  getUser(@Param('id', ParseIntPipe) id: number): { id: number } {
-    return { id };
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.userService.findOne(id);
+  }
+
+  @Post()
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.userService.create(createUserDto);
+  }
+
+  @Put(':id')
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateUserDto: UpdateUserDto,
+  ) {
+    return this.userService.update(id, updateUserDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.userService.remove(id);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,50 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  private users = [
+    {
+      id: 1,
+      name: 'John',
+      role: 'ADMIN',
+    },
+    {
+      id: 2,
+      name: 'Jane',
+      role: 'ADMIN',
+    },
+  ];
+
+  findAll() {
+    return this.users;
+  }
+
+  findOne(id: number) {
+    return this.users.find((user) => user.id === id);
+  }
+  create(createUserDto: CreateUserDto) {
+    const id = this.users.sort((a, b) => b.id - a.id)[0].id + 1;
+    const newUser = { id, ...createUserDto };
+    this.users.push(newUser);
+    return newUser;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    this.users = this.users.map((user) => {
+      if (user.id === id) {
+        return { ...user, ...updateUserDto };
+      }
+      return user;
+    });
+
+    return this.findOne(id);
+  }
+
+  remove(id: number) {
+    const user = this.findOne(id);
+    this.users = this.users.filter((user) => user.id !== id);
+    return user;
+  }
+}
