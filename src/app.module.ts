eb73be50e66f12188a3b95ce4ea1133bfb42e263
Rumar Gibs Ecaldre <margibs_hbb@yahoo.com>
2024-07-11 01:17:45 +0800

    Use global pipes and add swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { PostModule } from './post/post.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UserModule, PostModule],
+  imports: [ConfigModule.forRoot(), UserModule, PostModule],
   controllers: [AppController],
   providers: [AppService],
 })
