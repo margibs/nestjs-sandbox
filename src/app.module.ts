a5a1fecefd788d31bb354d5e5dc094339b2dc94d
Rumar Gibs Ecaldre <margibs_hbb@yahoo.com>
2024-07-10 20:53:51 +0800

    Full employee rest API

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { PostModule } from './post/post.module';
+import { DatabaseModule } from './database/database.module';
+import { EmployeeModule } from './employee/employee.module';
 
 @Module({
-  imports: [UserModule, PostModule],
+  imports: [UserModule, PostModule, DatabaseModule, EmployeeModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/employee/employee.controller.ts
@@ -0,0 +1,46 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  ParseIntPipe,
+  Query,
+} from '@nestjs/common';
+import { EmployeeService } from './employee.service';
+import { Prisma } from '@prisma/client';
+
+@Controller('employees')
+export class EmployeeController {
+  constructor(private readonly employeeService: EmployeeService) {}
+
+  @Post()
+  create(@Body() createEmployeeDto: Prisma.EmployeeCreateInput) {
+    return this.employeeService.create(createEmployeeDto);
+  }
+
+  @Get()
+  findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
+    return this.employeeService.findAll(role);
+  }
+
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.employeeService.findOne(id);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateEmployeeDto: Prisma.EmployeeUpdateInput,
+  ) {
+    return this.employeeService.update(id, updateEmployeeDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.employeeService.remove(id);
+  }
+}
